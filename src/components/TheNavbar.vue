10f796959285f005f85d62d1c60599542679de28
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2025-03-31 12:42:37 +0200

    first commit, create navbar

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav
+    class="border-1 border-[#6971A229] text-[#C1C2D3] w-fit rounded-[13px] mx-auto py-5 bg-linear-to-r from-[#04071D] to-[#0C0E23] px-10 flex justify-around gap-7 sticky"
+  >
+    <a
+      href="about"
+      class="hover:text-white transition-all"
+      :class="{ 'text-white': current === 'about' }"
+      >About</a
+    >
+    <a
+      href="projects"
+      class="hover:text-white transition-all"
+      :class="{ 'text-white': current === 'projects' }"
+      >Projects</a
+    >
+    <a
+      href="testimonials"
+      class="hover:text-white transition-all"
+      :class="{ 'text-white': current === 'testimonials' }"
+      >Testimonials</a
+    >
+    <a
+      href="contact"
+      class="hover:text-white transition-all"
+      :class="{ 'text-white ': current === 'contact' }"
+      >Contact</a
+    >
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    current: {
+      type: String,
+      default: "about",
+    },
+  },
+};
+</script>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="bg-[#000319] text-white h-[100vh] pt-10">
+    <TheNavbar current="about" />
+    <TheHeader />
+    <TheMainSection />
+    <TheRecentProjectsSection />
+    <TheExperienceSection />
+    <TheApproachSection />
+    <TheFeedbackSection />
+    <TheFooter />
+  </div>
+</template>
+
+<script>
+import TheApproachSection from "@/components/TheApproachSection.vue";
+import TheExperienceSection from "@/components/TheExperienceSection.vue";
+import TheFeedbackSection from "@/components/TheFeedbackSection.vue";
+import TheFooter from "@/components/TheFooter.vue";
+import TheHeader from "@/components/TheHeader.vue";
+import TheMainSection from "@/components/TheMainSection.vue";
+import TheNavbar from "@/components/TheNavbar.vue";
+import TheRecentProjectsSection from "@/components/TheRecentProjectsSection.vue";
+
+export default {
+  components: {
+    TheNavbar,
+    TheHeader,
+    TheMainSection,
+    TheRecentProjectsSection,
+    TheFeedbackSection,
+    TheExperienceSection,
+    TheApproachSection,
+    TheFooter,
+  },
+};
+</script>
