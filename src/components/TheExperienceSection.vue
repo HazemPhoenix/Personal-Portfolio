f2b945bbd367a62103cfd38a23e7ac68c93df0ac
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2025-04-02 23:31:43 +0200

    test

--- src/components/TheExperienceSection.vue
+++ src/components/TheExperienceSection.vue
@@ -27,25 +27,25 @@ export default {
           title: "Frontend Trainee",
           description:
             "Gained hands-on experience in frontend technologies and frameworks.",
-          src: "../../../public/svg/experience/_Group_.svg",
+          src: "../../../public/_Group_.svg",
         },
         {
           title: "Full Stack Developer Intern",
           description:
             "Worked on various projects, enhancing skills in both frontend and backend development.",
-          src: "../../../public/svg/experience/_Group_-1.svg",
+          src: "../../../public/_Group_-1.svg",
         },
         {
           title: "Backend Java Developer Intern",
           description:
             "Developed backend solutions using Java and Spring Boot.",
-          src: "../../../public/svg/experience/Frame 5.svg",
+          src: "../../../public/Frame 5.svg",
         },
         {
           title: "Backend Java Developer",
           description:
             "Contributed to backend development projects, focusing on Java and Spring Boot.",
-          src: "../../../public/svg/experience/Rectangle.svg",
+          src: "../../../public/Rectangle.svg",
         },
       ],
     };
