f91346a92c53d13b964945173a4df48543104671
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2025-04-03 04:59:49 +0200

    fix responsiveness in small mobile screens

--- src/components/experience/ExperienceCard.vue
+++ src/components/experience/ExperienceCard.vue
@@ -1,15 +1,19 @@
 <template>
   <div
-    class="h-[20vh] flex items-center gap-4 bg-gray-200 dark:bg-linear-to-r dark:from-[#04071D] dark:to-[#0C0E23] px-6 py-4 rounded-2xl border border-gray-400 dark:border-[#1e2230] shadow-lg"
+    class="h-[20vh] flex items-center gap-4 overflow-hidden bg-gray-200 dark:bg-linear-to-r dark:from-[#04071D] dark:to-[#0C0E23] px-6 py-4 rounded-2xl border border-gray-400 dark:border-[#1e2230] shadow-lg"
   >
     <div class="w-25 h-20 flex-shrink-0">
       <img :src="src" class="w-full h-full object-contain" />
     </div>
     <div class="flex flex-col">
-      <h2 class="text-gray-800 dark:text-white text-lg font-semibold">
+      <h2
+        class="text-gray-800 dark:text-white text-md md:text-lg font-semibold"
+      >
         {{ title }}
       </h2>
-      <p class="text-gray-600 dark:text-gray-400 text-sm">{{ description }}</p>
+      <p class="text-gray-600 dark:text-gray-400 text-xs md:text-sm">
+        {{ description }}
+      </p>
     </div>
   </div>
 </template>
