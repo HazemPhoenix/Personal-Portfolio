1ae14994b928cadb764cb5c1571a240224fef42c
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2025-04-03 04:53:19 +0200

    fix responsiveness in small mobile screens

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    class="border-1 border-[#6971A229] text-[#C1C2D3] w-full md:w-fit rounded-[13px] mx-auto py-5 bg-linear-to-r from-[#04071D] to-[#0C0E23] px-3 md:px-10 flex justify-around gap-4 md:gap-7 sticky top-10"
+    class="border-1 border-[#6971A229] text-[#C1C2D3] text-sm md:text-lg w-full md:w-fit rounded-[13px] mx-auto py-5 bg-linear-to-r from-[#04071D] to-[#0C0E23] px-3 md:px-10 flex justify-around gap-4 md:gap-7 sticky top-10"
   >
     <a href="#about" class="hover:text-white transition-all">About</a>
     <a href="#projects" class="hover:text-white transition-all">Projects</a>
